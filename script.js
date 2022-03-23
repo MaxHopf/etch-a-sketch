06e88e9a82f0403068c316db3932adb36037195e
Max Hopf <maxhopf@pm.me>
2022-03-23 21:22:50 +0100

    Add functions to create div grid
    *create div elements
    *display div elements in square parent div

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict"
+
+const squareRoot = 32;
+
+const sketchGrid = document.querySelector('#sketch-grid');
+
+function createSquares(squareRoot) {
+    for (let i = 0; i < squareRoot ** 2; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('style', 'background-color: red');
+        sketchGrid.appendChild(square);
+    }
+}
+
+function layoutSquaresToGrid(sketchGrid, squareRoot) {
+    sketchGrid.setAttribute('style', `display: grid; grid-template-columns: repeat(${squareRoot}, 1fr)`)
+}
+
+
+
