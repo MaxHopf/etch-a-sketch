dc9f8925587d965e141a304e8aafdbab6b75284b
Max Hopf <maxhopf@pm.me>
2022-03-23 23:06:29 +0100

    Add clear function
    *clear function to reset sketch div square

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 "use strict"
 
-const squareRoot = 32;
+let squareRoot = 32;
 
 const sketchGrid = document.querySelector('#sketch-grid');
+const clearBtn = document.querySelector('#clear');
+
+clearBtn.addEventListener('click', () => {
+    clearGrid();
+    createSquares(squareRoot);
+    layoutSquaresToGrid(sketchGrid, squareRoot)
+});
 
 function createSquares(squareRoot) {
     for (let i = 0; i < squareRoot ** 2; i++) {
         const squareElement = document.createElement('div');
+        squareElement.classList.add('element');
         hoverEvent(squareElement);
         sketchGrid.appendChild(squareElement);
     }
 }
 
 function layoutSquaresToGrid(sketchGrid, squareRoot) {
-    sketchGrid.setAttribute('style', `display: grid; grid-template-columns: repeat(${squareRoot}, 1fr)`)
+    sketchGrid.setAttribute('style', `display: grid; grid-template-columns: repeat(${squareRoot}, 1fr)`);
 }
 
 function hoverEvent(squareElement) {
@@ -21,6 +29,14 @@ function hoverEvent(squareElement) {
         squareElement.setAttribute('style', 'background-color: white');
     });
 }
+
+function clearGrid() {
+    const elements = document.querySelectorAll(".element");
+    elements.forEach(element => element.remove());
+}
+
+
+
 
 createSquares(squareRoot);
 layoutSquaresToGrid(sketchGrid, squareRoot)
